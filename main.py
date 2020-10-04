9a6bc020819f4961a3cd73e6b6cfec22789a7373
Nikolas Walker <zerkus10@gmail.com>
2020-10-04 17:25:02 -0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,23 @@
+
+get_password = input()  + 'q*s' #gets password from user and concatenates q*s
+password = ''
+
+for x in get_password:
+    if x == 'i':
+        password += '!'
+
+    elif x == 'a':
+        password += '@'
+
+    elif x == 'm':
+        password += 'M'
+
+    elif x == 'B':
+        password += '8'
+
+    elif x == 'o':
+        password += '.'
+    else:
+        password += x
+
+print(password)
